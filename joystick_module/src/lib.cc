231c9fe7e729f0079d17963a33ea0ea092adda2d
MechWipf <MechWipf@protonmail.com>
2025-01-26 12:50:42 +0100

    feat: Implement joystick handling via SDL2 (which is already included in gmod)

--- joystick_module/src/lib.cc
+++ joystick_module/src/lib.cc
@@ -1,11 +1,14 @@
 #include "GarrysMod/Lua/Interface.h"
 #include "GarrysMod/Lua/LuaBase.h"
 #include "joystick_module/src/lib.rs.h"
+#include <string.h>
 
 using namespace GarrysMod;
 using namespace GarrysMod::Lua;
+using namespace rust::cxxbridge1;
 
 const char* binaryversion = "1.2";
+static struct Evd *evd;
 
 int refresh(lua_State* state)
 {
@@ -14,32 +17,82 @@ int refresh(lua_State* state)
 
 int axis(lua_State* state)
 {
-    return 0;
+    int joy = state->luabase->GetNumber(1);
+    int axis = state->luabase->GetNumber(2);
+    auto value = evd->get_axis(joy, axis);
+
+    state->luabase->PushNumber(value);
+
+    return 1;
 }
 
 int button(lua_State* state)
 {
-    return 0;
+    int joy = state->luabase->GetNumber(1);
+    int button = state->luabase->GetNumber(2);
+    auto value = evd->get_button(joy, button);
+
+    state->luabase->PushBool(value);
+
+    return 1;
 }
 
 int pov(lua_State* state)
 {
-    return 0;
+    int joy = state->luabase->GetNumber(1);
+    int pov = state->luabase->GetNumber(2);
+    auto value = evd->get_pov(joy, pov);
+
+    state->luabase->PushNumber(value);
+
+    return 1;
 }
 
 int count(lua_State* state)
 {
-    return 0;
+    int joy = state->luabase->GetNumber(1);
+    int opt = state->luabase->GetNumber(2);
+    float out;
+
+    switch (opt)
+    {
+        case 1:
+            out = evd->get_num_axes(joy);
+            break;
+        case 2:
+            out = evd->get_num_povs(joy);
+            break;
+        case 3:
+            out = evd->get_num_buttons(joy);
+            break;
+        default:
+            out = evd->get_num_joysticks();
+            break;
+    }
+
+    state->luabase->PushNumber(out);
+
+    return 1;
 }
 
 int name(lua_State* state)
 {
-    return 0;
+    auto joy = state->luabase->GetNumber(1);
+    auto name = evd->get_joystick_name(joy);
+
+    state->luabase->PushString(name.c_str());
+
+    return 1;
 }
 
 int guidm(lua_State* state)
 {
-    return 0;
+    auto joy = state->luabase->GetNumber(1);
+    auto name = evd->get_joystick_guid(joy);
+
+    state->luabase->PushString(name.c_str());
+
+    return 1;
 }
 
 int restart(lua_State* state)
@@ -50,12 +103,18 @@ int restart(lua_State* state)
 
 int keyboardstate(lua_State* state)
 {
-    return 0;
+    // Dummy stub
+    if (strcmp(state->luabase->GetTypeName(1), "table")) {
+        state->luabase->CreateTable();
+        state->luabase->Push(-2);
+    }
+
+    return 1;
 }
 
 GMOD_MODULE_OPEN()
 {
-    auto evd = start();
+    evd = start().into_raw();
 
     LUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB);
     LUA->CreateTable();
@@ -77,5 +136,7 @@ GMOD_MODULE_OPEN()
 
 GMOD_MODULE_CLOSE()
 {
+    stop(evd);
+
     return 0;
 }
--- joystick_module/src/lib.rs
+++ joystick_module/src/lib.rs
@@ -1,41 +1,134 @@
-use std::{ mem::forget, thread};
+use std::mem::forget;
 
-use glob::glob;
-use gm_joystick_rs::{probe_device, read_events};
+use sdl2::JoystickSubsystem;
 
 #[cxx::bridge]
 mod ffi {
-extern "Rust" {
-        type Evd;
+    extern "Rust" {
         fn start() -> Box<Evd>;
+        unsafe fn stop(evd: *mut Evd);
+
+        type Evd;
+        fn get_num_joysticks(self: &Evd) -> f32;
+        fn get_joystick_name(self: &Evd, id: f32) -> String;
+        fn get_joystick_guid(self: &Evd, id: f32) -> String;
+        fn get_axis(&self, id: f32, id_axis: u32) -> f32;
+        fn get_button(&self, id: f32, id_btn: u32) -> bool;
+        fn get_pov(&self, id: f32, id_pov: u32) -> f32;
+        fn get_num_axes(&self, id: f32) -> f32;
+        fn get_num_buttons(&self, id: f32) -> f32;
+        fn get_num_povs(&self, id: f32) -> f32;
     }
 }
 
 struct Evd {
+    joystick: JoystickSubsystem,
 }
 
 fn start() -> Box<Evd> {
-    let mut threads = vec![];
+    let sdl2_context = sdl2::init().unwrap();
 
-    let devices = glob("/dev/input/by-id/*-event-joystick").expect("Failed to read glob pattern");
-    for entry in devices {
-        match entry {
-            Ok(path) => {
-                println!("{path:?}");
+    let joystick_module = sdl2_context.joystick().unwrap();
+    let evd = Evd {
+        joystick: joystick_module,
+    };
 
-                let t = thread::spawn(move || {
-                    let mut device = probe_device(&path);
-                    if device.name.contains("Microsoft") {
-                        read_events(&path, &mut device);
-                    }
-                });
-                threads.push(t);
-            }
-            Err(e) => println!("Error: {e:?}"),
+    forget(sdl2_context);
+    Box::new(evd)
+}
+
+fn stop(evd: *mut Evd) {
+    if evd.is_null() {
+        return;
+    }
+
+    unsafe {
+        _ = Box::from_raw(evd);
+    }
+}
+
+impl Evd {
+    fn get_num_joysticks(&self) -> f32 {
+        if let Ok(num_joysticks) = self.joystick.num_joysticks() {
+            num_joysticks as f32
+        } else {
+            0f32
         }
     }
 
-    forget(threads);
+    fn get_joystick_name(&self, id: f32) -> String {
+        if let Ok(name) = self.joystick.name_for_index(id as _) {
+            name
+        } else {
+            "".to_owned()
+        }
+    }
 
-    Box::new(Evd {})
+    fn get_joystick_guid(&self, id: f32) -> String {
+        if let Ok(name) = self.joystick.device_guid(id as _) {
+            name.string()
+        } else {
+            "".to_owned()
+        }
+    }
+
+    fn get_axis(&self, id: f32, id_axis: u32) -> f32 {
+        if let Ok(device) = self.joystick.open(id as _) {
+            if let Ok(axis) = device.axis(id_axis) {
+                axis as f32
+            } else {
+                0f32
+            }
+        } else {
+            0f32
+        }
+    }
+
+    fn get_button(&self, id: f32, id_btn: u32) -> bool {
+        if let Ok(device) = self.joystick.open(id as _) {
+            if let Ok(btn) = device.button(id_btn) {
+                btn
+            } else {
+                false
+            }
+        } else {
+            false
+        }
+    }
+
+    fn get_pov(&self, id: f32, id_pov: u32) -> f32 {
+        if let Ok(device) = self.joystick.open(id as _) {
+            if let Ok(pov) = device.hat(id_pov) {
+                pov as i32 as f32
+            } else {
+                0f32
+            }
+        } else {
+            0f32
+        }
+    }
+
+    fn get_num_axes(&self, id: f32) -> f32 {
+        if let Ok(device) = self.joystick.open(id as _) {
+            device.num_axes() as f32
+        } else {
+            0f32
+        }
+    }
+
+    fn get_num_buttons(&self, id: f32) -> f32 {
+        if let Ok(device) = self.joystick.open(id as _) {
+            device.num_buttons() as f32
+        } else {
+            0f32
+        }
+    }
+
+    fn get_num_povs(&self, id: f32) -> f32 {
+        if let Ok(device) = self.joystick.open(id as _) {
+            device.num_hats() as f32
+        } else {
+            0f32
+        }
+    }
 }
